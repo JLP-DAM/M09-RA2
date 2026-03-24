050c260343c51a80a0552494d294a5802e8f4ea9
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2026-03-24 14:48:07 +0100

    11-Barber acabat

--- 11-Barber_mandrós/src/Barber.java
+++ 11-Barber_mandrós/src/Barber.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class Barber extends Thread {
     public Barber(String nom) {
         this.setName(nom);
@@ -9,26 +7,22 @@ public class Barber extends Thread {
     public void run() {
         Barberia barberia = Barberia.getInstancia();
 
-        Random random = new Random();
-
         while (true) {
             if (barberia.getSalaEspera().size() <= 0) {
                 try {
                     System.out.println("Ningú en espera");
-                    System.out.println("Barber " + this.getName());
-                    wait();
+                    System.out.println("Barber " + this.getName() + " dormint");
+
+                    synchronized (barberia.getCondBarber()) {
+                        barberia.getCondBarber().wait();
+                    }
+
                 } catch(Exception exception) {}
             }
 
             Client client = barberia.seguentClient();
 
-            if (client == null) {continue;}
-
-            synchronized(this) {
-                try {
-                    this.sleep(900 + random.nextLong(0, 100));
-                } catch(Exception exception) {}
-            }
+            client.tallarseElCabell();
         }
     }
 }
--- 11-Barber_mandrós/src/Client.java
+++ 11-Barber_mandrós/src/Client.java
@@ -1,15 +1,24 @@
+import java.util.Random;
+
 public class Client {
     private String nom;
 
     public Client(int id) {
-        nom = "Client-" + id;
-    }
-
-    public String getNom() {
-        return nom;
+        this.nom = "Client-" + id;
     }
 
     public void tallarseElCabell() {
-        
+        Random random = new Random();
+
+        System.out.println("Li toca al client " + this.getNom());
+        System.out.println("Tallant cabell a " + this.getNom());
+
+        try {
+            Thread.sleep(900 + random.nextInt(0, 100));
+        } catch (Exception exception) {}
+    }
+
+    public String getNom() {
+        return this.nom;
     }
 }
